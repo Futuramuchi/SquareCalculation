708e896261edb5100ddabc4695cf66ec1bdb2d53
Futuramuchi <Yana130302@gmail.com>
2021-09-23 22:28:25 +0300

    updated unit tests

--- SquareTestTests/UnitTests.cs
+++ SquareTestTests/UnitTests.cs
@@ -14,15 +14,35 @@ namespace SquareTest.Tests
         [TestMethod()]
         public void IsTriangleIsRight()
         {
-            //arrange
+
             string isTriangleRight = "Right triangle: 30";
 
-            //act
             Triangle triangle = new Triangle();
             string actual = triangle.CountTriangleSquare(12, 13, 5);
 
-            //assert
             Assert.AreEqual(isTriangleRight, actual);
+        }
+
+        [TestMethod()]
+        public void IsCorrectTriangleSquare() 
+        {
+            string expected = "6";
+
+            Triangle triangle = new Triangle();
+            string actual = triangle.CountTriangleSquare(3, 4, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsCorrectCircleSquare() 
+        {
+            double expected = 78.54;
+
+            Circle circle = new Circle();
+            double actual = circle.CountCircleSquare(5);
+
+            Assert.AreEqual(expected, actual);
         }
 
     }
